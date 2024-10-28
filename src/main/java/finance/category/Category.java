f8647e5831a3b407919f26dc6af953095acae672
Trunki <hahn.silvan@gmx.ch>
2024-10-28 15:42:25 +0100

    adding Category java class

---
+++ src/main/java/finance/category/Category.java
@@ -0,0 +1,24 @@
+package finance.category;
+
+public class Category {
+    private String name;
+    private String description;
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{name='" + name + "', description='" + description + "'}";
+    }
+}
