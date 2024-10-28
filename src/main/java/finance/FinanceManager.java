d85a3d2ffa6b68b4a570b0e738ec570084a64273
Trunki <hahn.silvan@gmx.ch>
2024-10-28 14:37:43 +0100

    Adding Main script (FinanceManager)

---
+++ src/main/java/finance/FinanceManager.java
@@ -0,0 +1,20 @@
+public class FinanceManager {
+    private static FinanceManager instance;
+
+    // Private Konstruktor für Singleton Pattern
+    private FinanceManager() {
+    }
+
+    // Methode zum Zugriff auf die Singleton-Instanz
+    public static FinanceManager getInstance() {
+        if (instance == null) {
+            instance = new FinanceManager();
+        }
+        return instance;
+    }
+
+    // Einstiegspunkt für das Programm
+    public static void main(String[] args) {
+        System.out.println("Finanzverwaltungssystem gestartet!");
+    }
+}
