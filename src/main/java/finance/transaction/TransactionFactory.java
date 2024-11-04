64c01574a7370ff675ef47a1dcb413f73086371d
Trunki <hahn.silvan@gmx.ch>
2024-11-04 14:35:00 +0100

    Refactored FinanceManagerTest to use Category objects for transactions and added InvalidDateException handling in tests.

--- src/main/java/finance/transaction/TransactionFactory.java
+++ src/main/java/finance/transaction/TransactionFactory.java
@@ -1,13 +1,15 @@
 package finance.transaction;
 
+import finance.category.Category;
 import finance.exceptions.InvalidTransactionException;
+import finance.exceptions.InvalidDateException;
 
 public class TransactionFactory {
-    public static Transaction createTransaction(String type, double amount, String category, String date, String description) throws InvalidTransactionException {
+    public static Transaction createTransaction(String type, double amount, Category category, String date, String description) throws InvalidTransactionException, InvalidDateException {
         return switch (type.toLowerCase()) {
             case "income" -> new Income(amount, category, date, description);
             case "expense" -> new Expense(amount, category, date, description);
-            default -> throw new InvalidTransactionException("Unbekannter Transaktionstyp: " + type);
+            default -> throw new InvalidTransactionException("Unknown transaction type: " + type);
         };
     }
 }
